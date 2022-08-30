abb4897c12c5767cd1e23681527ef7b8e70c372a
Billy McGrath <bmcgrath@matterport.com>
2022-08-29 21:18:21 -0700

    more changes still broken

--- main/test/test_hello_world_main.c
+++ main/test/test_hello_world_main.c
@@ -1,6 +1,14 @@
 #include "unity.h"
 #include "cmock.h"
 
+// try mocking the other headers even though we don't care to use them
+// #include <stdio.h>
+// #include "mock_sdkconfig.h"
+// #include "mock_freertos/FreeRTOS.h"
+// #include "mock_freertos/task.h"
+// #include "mock_esp_chip_info.h"
+// #include "mock_esp_flash.h"
+
 #include "mock_hey_ceedling.h"
 
 #include "hello_world_main.h"
